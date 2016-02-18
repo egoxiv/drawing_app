c8f47c8ad29029f92d235924e28e2f072d9600d2
Erik Gomez <egoxiv@gmail.com>
2016-02-18 11:36:25 -0800

    Added color selector

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
-var canvas = document.getElementsByTagName("canvas")[0];
-var context = canvas.getContext("2d");
+var canvas = document.getElementsByTagName("canvas")[0],
+context    = canvas.getContext("2d"),
+color;
 //When clicking on control list items
 $("#colors").on("click", "li", function(){
 	//Deselect sibling elements
@@ -9,6 +10,9 @@ $("#colors").on("click", "li", function(){
 	$(this).addClass("selected");
 
 	//cache current color
-
+	color = $(this).css("background-color");
 })
 
+$("#toggleNewColor").on("click", function() {
+	$("#colorSelect").toggle();
+})
